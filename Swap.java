eafeb35ee48d77e28bd7e77bb124078bd934c9db
Ajith Bobby <74547561+ajithbobby@users.noreply.github.com>
2022-01-01 14:47:36 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+class Swap
+{ public static void main(String[] args)
+  { int a=15,b=17,temp;
+    System.out.println("Swapping of two numbers a="+a +"&b="+b);
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("After Swapping");
+    System.out.println("a="+a);
+    System.out.println("b="+b);
+  }
+}
